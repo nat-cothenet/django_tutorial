5e61bb16dae3285f38ee82d5ee3246461cb09ff4
natbodington <natalie.bodington@gmail.com>
2022-06-02 19:17:53 -0700

    Adding user auth

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model."""
@@ -13,7 +15,7 @@ class Topic(models.Model):
 class Entry(models.Model):
     """Something specific learned about a topic."""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    text = models.TextField()
+    text = models.TextField(default="", blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
