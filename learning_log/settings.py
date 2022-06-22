57ebffd3bf0cd78a54dd01e35b9bd9c25b79e412
natbodington <natalie.bodington@gmail.com>
2022-06-22 13:55:16 -0700

    Changing secret name

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
